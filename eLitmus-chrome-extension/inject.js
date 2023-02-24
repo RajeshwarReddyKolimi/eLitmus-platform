9a86af077babaeb4a01f5764f222a74f470cf105
RajeshwarReddyKolimi <90194850+RajeshwarReddyKolimi@users.noreply.github.com>
2023-02-24 19:53:48 +0530

    modified code

---
+++ eLitmus-chrome-extension/inject.js
@@ -0,0 +1,41 @@
+
+
+async function init() {
+    alert("you're now being monitored, do not switch tabs")
+    var video = document.createElement('video');
+    video.autoplay = true;
+    video.playsInline = true;
+    video.controls = false;
+    var canvas = document.createElement('canvas');
+    const constraints = { 'video': true, 'audio': false };
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    video.srcObject = stream;
+    var ctx = canvas.getContext("2d");
+    ctx.canvas.width = 640;
+    ctx.canvas.height = 480;
+    setTimeout(() => {
+        ctx.drawImage(video, 0, 0, ctx.canvas.width, ctx.canvas.height);
+        function sendPic(params) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+
+            };
+            xhttp.open("POST", "http://localhost:8081/student-upload", true);
+            xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+            xhttp.setRequestHeader('Content-type', 'application/json');
+
+            xhttp.send(JSON.stringify({ mail: JSON.parse(sessionStorage.getItem("user")), data: canvas.toDataURL() }));
+            setTimeout(() => {
+                sendPic()
+            }, 180000);
+        }
+        sendPic()
+    }, 1000);
+}
+
+init();
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        sessionStorage.setItem("user", JSON.stringify(request))
+    }
+);
---
+++ eLitmus-chrome-extension/popup.js
@@ -0,0 +1,41 @@
+
+document.getElementById("clickactivity").addEventListener("click", () => {
+    var xhttp = new XMLHttpRequest();
+    var name = document.getElementById("name").value;
+    var mail = document.getElementById("email").value;
+    var code = document.getElementById("code").value;
+    xhttp.onreadystatechange = function () {
+        getCurrentTab().then((tab) => {
+            injectContentScript(tab[0], mail);
+        })
+    };
+    xhttp.open("POST", "http://localhost:8081/create-student", true);
+    xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhttp.setRequestHeader('Content-type', 'application/json');
+
+    xhttp.send(JSON.stringify({ name: name, code: code, mail: mail }));
+
+
+});
+function injectContentScript(tab, data) {
+    const { id, url } = tab;
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: id, allFrames: true },
+            files: ['inject.js']
+        }
+    )
+
+    setTimeout(async () => {
+        const [tab] = await chrome.tabs.query({ active: true });
+        chrome.tabs.sendMessage(tab.id, data);
+        window.close();
+    }, 2000);
+
+}
+function getCurrentTab() {
+    return new Promise((res, rej) => {
+        let queryOptions = { active: true };
+        chrome.tabs.query(queryOptions, (data) => { res(data) })
+    })
+}

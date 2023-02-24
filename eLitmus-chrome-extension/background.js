133909e515e5cdbef226742c63da621dcfe9a048
RajeshwarReddyKolimi <90194850+RajeshwarReddyKolimi@users.noreply.github.com>
2023-02-24 19:52:40 +0530

    Delete eLitmus-chrome-extension directory

--- eLitmus-chrome-extension/background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener(function (tab) {
-
-  // for the current tab, inject the "inject.js" file &amp; execute it	
-  chrome.scripting.executeScript({
-
-    files: ['inject.js']
-  });
-});
